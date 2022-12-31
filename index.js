c0898405ce320f11e5009a6e26c41128144b930c
Daniil Ermolovich <ermolovichd007@gmail.com>
2022-12-31 17:06:17 +0300

    end task

---
+++ index.js
@@ -0,0 +1,24 @@
+const generatePhrase = document.querySelector('.generatephase');
+const genetateTextCopy = document.querySelector('.genetateTextCopy');
+var copy = '';
+
+async function genetateText() {
+  const phrases = 'phrasesData.json';
+  const res = await fetch(phrases);
+  const data = await res.json();
+  generatePhrase.innerHTML = `С новым годом, ${
+    data.happyNewYear[getRandomInt(data.happyNewYear.length)]
+  } Я желаю ${data.iWish[getRandomInt(data.iWish.length)]} И пусть ${
+    data.andLet[getRandomInt(data.andLet.length)]
+  }`;
+  copy = generatePhrase.innerHTML;
+  genetateTextCopy.style.display = 'block';
+}
+
+function copyText() {
+  window.navigator.clipboard.writeText(copy);
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
